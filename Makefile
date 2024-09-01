40cbbc1a08b962d9a551ccb0150ff6713422ba0e
mircea007 <mirceareb@gmail.com>
2024-09-01 23:05:18 +0300

    stack functionality, backspace works

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+.DEFAULT_GOAL := bin/main
+
 bin:
 	mkdir bin
 
--- main.c
+++ main.c
@@ -5,16 +5,57 @@
 
 #include <ncurses.h>
 
+#define false 0
+#define true 1
+#define bool int
+
 // draw screen and at the end flush curses internal buffers to the screen
+
+char buf[1000];
+int bufpoz = 0;
+
+bool editor_exit = false;
 
 void draw() {
   clear();
-  move( 0, 0 ); // start drawing scene from the top-left
 
-  if( false && (clock() & 1) )
-    printw( "Hello World !!!" );
-  else
-    printw( "Hello World 2 !!!" );
+  // not in main since window size can change
+  int width, height;
+  getmaxyx( stdscr, height, width );
+
+  // process new key events
+  char ch;
+  char bigboss = '?';
+  //int iter = 0;
+  while( /* iter < 10 && */ (ch = getch()) != EOF ) {
+    //iter++;
+
+    if( ch == 'q' )
+      editor_exit = true;
+    
+    if( ch == KEY_BACKSPACE || ch == 127 ){
+      if( bufpoz >= 0 )
+        bufpoz--;
+    }else{
+      if( bufpoz < 1000 )
+        buf[bufpoz++] = ch;
+      bigboss = ch;
+      //exit( 1 ); // sa vedem daca se ajunge aici
+    }
+  }
+
+  move( 0, 0 );
+  printw( "Terminal size: %dx%d (bigboss = %c | %d)\n", height, width, bigboss, int(bigboss) );
+
+  // draw status line
+  for( int c = 0; c < width; c++ )
+    mvaddch( height - 2, c, '-' );
+
+  buf[bufpoz] = '\0';
+  move( 1, 0 );
+  char *ptr = buf;
+  while( *ptr )
+    addch( *(ptr++) );
 
   refresh();
 }
@@ -24,11 +65,16 @@ int main( int argc, char **argv ) {
   cbreak(); // change to raw() when we dont want Ctrl+C, Ctrl+D to do anything
   noecho(); // we don't want to not be able to control what is on the screen
 
+  timeout( 0 ); // non-blocking read (getch())
+
   do{
     draw();
-    usleep( 10000 );
-  }while( true );
+    usleep( 1000 * 20 );
+  }while( !editor_exit );
 
   endwin();
+
+  buf[bufpoz] = '\0';
+  printf( "%s\n", buf );
   return 0;
 }
