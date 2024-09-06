b434d7bab475365abfbf13eb499299c59a5a2127
mircea007 <mirceareb@gmail.com>
2024-09-06 17:16:19 +0300

    line edit buffer remake complete, good movement

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 .DEFAULT_GOAL := bin/main
 
+DEBUG ?= 1
+ifeq ($(DEBUG), 1)
+    EXTRA_FLAGS = -Wall -Wextra -O0 -g -fsanitize=address -fsanitize=undefined
+else
+    EXTRA_FLAGS = -Wall -O2
+endif
+
 bin:
 	mkdir bin
 
 bin/main: bin main.cpp
-	g++ -Wall -O2 -lcurses -o bin/main main.cpp
+	g++ -lncurses $(EXTRA_FLAGS) -o bin/main main.cpp
--- main.cpp
+++ main.cpp
@@ -3,10 +3,108 @@
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
+#include <assert.h>
 
 #include <ncurses.h>
 
-// draw screen and at the end flush curses internal buffers to the screen
+template<typename T> T min( T a, T b ) { return a < b ? a : b; }
+template<typename T> T max( T a, T b ) { return a > b ? a : b; }
+
+template<typename T, const int HARD_LIMIT> struct LimitVector {
+  static const int MIN_NONZERO_SIZ = 8;
+  T *mem;
+  int mem_siz;
+  int siz;
+
+  LimitVector(): mem(nullptr), mem_siz(0), siz(0) {}
+  LimitVector( int siz ): mem_siz(siz), siz(siz) {
+    mem = new T[siz];
+  }
+
+  ~LimitVector() {
+    if( mem )
+      delete []mem;
+  }
+
+  LimitVector& operator = ( const LimitVector& that ) {
+    if( mem )
+      delete []mem;
+
+    mem_siz = that.mem_siz;
+    siz = that.siz;
+
+    if( that.mem ){
+      mem = new T[mem_siz];
+      for( int i = 0; i < siz; i++ )
+        mem[i] = that.mem[i];
+    }
+
+    return *this;
+  }
+
+  T& operator []( const int idx ) {
+    //assert( 0 <= idx < siz );
+    return mem[idx];
+  }
+  
+  inline int size() { return siz; }
+  inline bool empty() { return !siz; }
+
+  void clear() {
+    if( mem )
+      delete []mem;
+
+    mem = nullptr;
+    mem_siz = 0;
+    siz = 0;
+  }
+
+  void expand() {
+    assert( mem_siz < HARD_LIMIT );
+    
+    int new_siz = min( max( MIN_NONZERO_SIZ, mem_siz * 2 ), HARD_LIMIT );
+    T *new_mem = new T[new_siz];
+
+    for( int i = 0; i < siz; i++ )
+      new_mem[i] = mem[i];
+    
+    if( mem )
+      delete []mem;
+    mem = new_mem;
+    mem_siz = new_siz;
+  }
+
+  void push_back( T el ) {
+    if( siz >= mem_siz )
+      expand();
+
+    mem[siz++] = el;
+  }
+
+  void pop_back() {
+    //assert( siz > 0 );
+    siz--;
+  }
+
+  void insert( int idx, T el ) {
+    //assert( 0 <= idx <= siz );
+    if( siz >= mem_siz )
+      expand();
+
+    for( int i = siz - 1; i >= idx; i-- ) // memmove
+      mem[i + 1] = mem[i];
+    mem[idx] = el;
+    siz++;
+  }
+
+  void erase( int idx ) {
+    //assert( 0 <= idx < siz );
+
+    for( int i = idx; i + 1 < siz; i++ ) // memmove
+      mem[i] = mem[i + 1];
+    siz--;
+  }
+};
 
 bool editor_exit = false;
 
@@ -14,108 +112,149 @@ bool editor_exit = false;
 // - random insert/delete, cursor tracking, searching
 // also: we would like this to not have a huge memory overhead since we intend to edit big files
 
-// struct Line {
-//   std::vector<char> line; // maybe implement by myself??
-//   Line(): line(0) {}
+const int MAX_LINE_SIZ = 2000;
+struct Line {
+  LimitVector<char, MAX_LINE_SIZ> line;
+  Line(): line(0) {}
 
-//   // line ne spune unde sa desenam, begin_poz ne spune ce interval sa afisam din string
-//   void draw( int line, int begin_poz ) {
-    
-//   }
-// };
+  Line& operator = ( const Line& that ) {
+    line = that.line;
+    return *this;
+  }
+
+  // i just don't want to do inheritance
+  inline void insert( int idx, char ch ) { line.insert( idx, ch ); }
+  inline void erase( int idx ) { line.erase( idx ); }
+  inline int size() { return line.size(); }
+  inline void pop_back() { line.pop_back(); }
+
+  // line ne spune unde sa desenam, begin_poz ne spune ce interval sa afisam din string
+  void draw( int y, int begin_poz, int len ) {
+    move( y, 0 );
+
+    int end_poz = min( begin_poz + len, (int)line.size() );
+    for( int idx = begin_poz; idx < end_poz; idx++ )
+      addch( line[idx] );
+  }
+};
 
 const int FILE_BUFSIZ = 20000;
 const int MAX_LINES = FILE_BUFSIZ;
 
+// this structure should eventualy also hold information about how to save the buffer (fillepath/format etc..)
 struct EditBuffer {
-  char *buf; // extra space for '\0'
-  int bufpoz;
-  int cursor;
+  LimitVector<Line, MAX_LINES> lines;
 
-  // buf has bufpoz characters, buf[bufpoz] == '\0' allways, 0 <= cursor <= bufpoz
+  int cursor_line;
+  int cursor_poz;
 
-  EditBuffer(): bufpoz(0), cursor(0) {
-    buf = new char[FILE_BUFSIZ + 1];
-    buf[0] = '\0';
-  }
+  EditBuffer(): lines(1), cursor_line(0), cursor_poz(0) { lines[0] = Line(); }
+  ~EditBuffer() {}
 
-  ~EditBuffer() {
-    delete []buf;
-  }
-
-  void insert_char( char ch, int poz ) {
-    if( poz < 0 || poz > bufpoz )
-      return;
-    
-    if( bufpoz >= FILE_BUFSIZ ) // make sure to not go over buffer limit (no resizing yet)
-      return;
-    
-    memmove( buf + poz + 1, buf + poz, (bufpoz + 1) - (poz) );
-    buf[poz] = ch;
-    bufpoz++;
+  void normalize_cursor() {
+    cursor_poz = min( lines[cursor_line].size(), cursor_poz );
   }
   
   void insert_char_cursor( char ch ) {
-    insert_char( ch, cursor );
-    cursor++; // daca insert_char nu face nimic asta e o greseala, schimb mai tarziu..
-  }
-
-  void remove_char( int poz ) {
-    if( poz < 0 || poz >= bufpoz )
-      return;
+    normalize_cursor();
     
-    memmove( buf + poz, buf + poz + 1, (bufpoz + 1) - (poz + 1) );
-    bufpoz--;
+    if( ch == '\n' ){
+      lines.insert( cursor_line + 1, Line() );
+
+      for( int i = cursor_poz; i < lines[cursor_line].size(); i++ )
+        lines[cursor_line + 1].line.push_back( lines[cursor_line].line[i] );
+
+      // don't want to do this by hand just because
+      for( ; lines[cursor_line].size() > cursor_poz ; )
+        lines[cursor_line].pop_back();
+      
+      cursor_line++;
+      cursor_poz = 0;
+      return;
+    }
+    
+    lines[cursor_line].insert( cursor_poz++, ch );
   }
 
   // removes the character to the left of the cursor
   void backspace() {
-    if( cursor <= 0 )
+    normalize_cursor();
+
+    if( cursor_poz > 0 ){
+      cursor_poz--;
+      lines[cursor_line].erase( cursor_poz );
       return;
-    
-    cursor--;
-    remove_char( cursor );
+    }
+
+    if( cursor_line <= 0 )
+      return;
+
+    cursor_poz = lines[cursor_line - 1].size();
+    cursor_line--;
+
+    for( int i = 0; i < lines[cursor_line + 1].size(); i++ )
+      lines[cursor_line].line.push_back( lines[cursor_line + 1].line[i] );
+    lines.erase( cursor_line + 1 );
   }
 
   // cursor movement
   void cursor_left() {
-    if( cursor <= 0 )
+    normalize_cursor();
+    if( cursor_line <= 0 && cursor_poz <= 0 )
       return;
-
-    cursor--;
+    
+    if( cursor_poz > 0 )
+      cursor_poz--;
+    else{
+      cursor_line--;
+      cursor_poz = lines[cursor_line].size();
+    }
   }
 
   void cursor_right() {
-    if( cursor + 1 > bufpoz )
+    normalize_cursor();
+    if( (cursor_line + 1 >= lines.size()) && (cursor_poz >= lines[cursor_line].size()) )
       return;
 
-    cursor++;
+    if( cursor_poz + 1 <= lines[cursor_line].size() )
+      cursor_poz++;
+    else{
+      cursor_line++;
+      cursor_poz = 0;
+    }
   }
 
-  void cursor_up() {}
-  void cursor_down() {}
+  void cursor_up() {
+    if( cursor_line <= 0 )
+      return;
+
+    cursor_line--;
+  }
+
+  void cursor_down() {
+    if( cursor_line + 1 >= lines.size() )
+      return;
+
+    cursor_line++;
+  }
 
   // draws the buffer onto the screen, will add:
   //  - frame start cursor
   //  - frame coordinates (startl, startc, width, height)
-  void draw() {
+  void draw( int width, int height ) {
     move( 0, 0 );
-    for( int i = 0; i < cursor; i++ )
-      addch( buf[i] );
 
-    // save cursor position
-    int cursorx, cursory;
-    getyx( stdscr, cursorx, cursory );
+    int last_line = min( lines.size(), height );
+    for( int i = 0; i < last_line; i++ )
+        lines[i].draw( i, 0, width );
 
-    for( int i = cursor; i < bufpoz; i++ )
-      addch( buf[i] );
-
-    move( cursorx, cursory );
+    move( cursor_line, min( lines[cursor_line].size(), cursor_poz ) );
   }
 };
 
 EditBuffer test_buf;
 
+// draw screen and at the end flush curses internal buffers to the screen
 char bigboss = '?';
 void draw() {
   clear();
@@ -170,7 +309,7 @@ void draw() {
   for( int c = 0; c < width; c++ )
     mvaddch( height - 2, c, '-' );
 
-  test_buf.draw();
+  test_buf.draw( width, height - 2 ); // we don't want the buffer to touch the status line
   refresh();
 }
 
@@ -184,6 +323,9 @@ int main( int argc, char **argv ) {
 
   timeout( 0 ); // non-blocking read (getch())
   keypad( stdscr, true );
+
+  // at the end we move the cursor to its position in the buffer
+  //curs_set( 0 ); // invisible cursor
   
   do{
     draw();
